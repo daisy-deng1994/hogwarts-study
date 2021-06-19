388bdf94cc8ab982414c1e6961ac71caa17ef004
daisy-deng1994 <1065962322@qq.com>
2021-06-19 23:50:18 +0800

    简化文件夹

---
+++ code/calculator.py
@@ -0,0 +1,16 @@
+# editor:Daisy
+# date:2021/6/19 4:25 下午
+
+
+class Calculator:
+    def add(self,a,b):
+        return a + b
+
+    def sub(self,a,b):
+        return a - b
+
+    def mul(self,a,b):
+        return a * b
+
+    def div(self,a,b):
+        return a / b
