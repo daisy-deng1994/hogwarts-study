fc2f022d3a12852319ab0f35d0d8fb54eac4f6f8
daisy-deng1994 <1065962322@qq.com>
2021-06-20 10:43:12 +0800

    1.更新取数逻辑 2.优化精度问题

--- testcases/test_calc.py
+++ testcases/test_calc.py
@@ -11,7 +11,7 @@ from homework.code.calculator import Calculator
 def get_calc_data(n : str):
     with open('../data/calc_data.yml', 'r', encoding='utf-8') as c:
         total = yaml.safe_load(c)
-    return total.get(n),(b[3] for b in total.get(n))
+    return total.get(n).get('datas'),total.get(n).get('ids')
 
 
 @allure.feature("计算器功能测试")
@@ -31,27 +31,35 @@ class TestCalculator:
 
     @allure.story("加法功能")
     @allure.step("加法正向用例")
-    @pytest.mark.parametrize('a,b,expect', datas=get_calc_data("add_success")[0], ids=get_calc_data("add_success")[1])
+    @pytest.mark.parametrize('a,b,expect', get_calc_data("add_success")[0], ids=get_calc_data("add_success")[1])
     def test_add_success(self, a, b, expect):
-        assert expect == self.calc.add(a, b)
+        if type(self.calc.add(a,b))==float:
+            aa=float(format(self.calc.add(a,b),'.2f'))
+            assert expect == aa
+        else:
+            assert expect == self.calc.add(a, b)
 
     @allure.step("加法反向用例")
-    @pytest.mark.parametrize('a,b,expect', datas=get_calc_data("add_fail")[0], ids=get_calc_data("add_fail")[1])
+    @pytest.mark.parametrize('a,b,expect', get_calc_data("add_fail")[0], ids=get_calc_data("add_fail")[1])
     def test_add_fail(self, a, b, expect):
-        with pytest.raises('TypeError') as s:
+        with pytest.raises(TypeError) as s:
             self.calc.add(a, b)
             assert expect == str(s.value)
 
     @allure.story("除法功能")
     @allure.step("除法正向用例")
-    @pytest.mark.parametrize('a,b,expect', datas=get_calc_data("div_success")[0], ids=get_calc_data("div_success")[1])
-    def test_div(self, a, b, expect):
-        assert expect == self.calc.div(a, b)
+    @pytest.mark.parametrize('a,b,expect', get_calc_data("div_success")[0], ids=get_calc_data("div_success")[1])
+    def test_div_success(self, a, b, expect):
+        if type(self.calc.div(a,b))==float:
+            aa=float(format(self.calc.div(a,b),'.3f'))
+            assert expect == aa
+        else:
+            assert expect == self.calc.div(a, b)
 
     @allure.step("除法反向用例")
-    @pytest.mark.parametrize('a,b,expect', datas=get_calc_data("div_fail")[0], ids=get_calc_data("div_fail")[1])
-    def test_div(self,a,b,expect):
-        with pytest.raises('ZeroDivisionError' or "TypeError") as s:
+    @pytest.mark.parametrize('a,b,expect', get_calc_data("div_fail")[0], ids=get_calc_data("div_fail")[1])
+    def test_div_fail(self,a,b,expect):
+        with pytest.raises(ZeroDivisionError or TypeError) as s:
             self.calc.add(a, b)
             assert expect == str(s.value)
 
